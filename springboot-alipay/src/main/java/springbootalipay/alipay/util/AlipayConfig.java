928f0769fc398f11c1e5a0d1c5f1109bc48311c3
marryCrase <1127206278@qq.com>
2020-07-31 13:56:38 +0800

    springboot支付宝支付 退款 订单查询（带数据库）

---
+++ springboot-alipay/src/main/java/springbootalipay/alipay/util/AlipayConfig.java
@@ -0,0 +1,14 @@
+package springbootalipay.alipay.util;
+public class AlipayConfig {
+    // 应用ID,您的APPID，收款账号既是您的APPID对应支付宝账号
+    public static String app_id = "2021000116686515";
+    // 商户私钥，您的PKCS8格式RSA2私钥
+    public static String merchant_private_key = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCyOb/31p7UD/RLsrO+GKrupUJ6q7w+5k1MIwOYjpmdJaL7Y8doft1xgeIaJJcs+yiCmO5jiWFXzWFYK7lrGrT2aBOJaQb3jw4oTqHkNypYvyNjXOESwFAoKPDj7N8ss7ZK032A0wMauFgXtyDSjn0taIq6NQnMeHpGKhOVfyb/3G14iwp6uoSLbTvufaHyfJGun3E702jGbXJAGuqEEaYBGVHBn22mHOvtlVfwa244+fRgSQaiGOUxe5mg9T1HU0uoV9iLTcW4VtjVwCP2HkKE9TGjs73G4C3JSXFwRsYcUoQWj1xuhtENJ6vRNMt3RVb+NG9bW3ZY3lY4uRQ4zC6lAgMBAAECggEBAJZmZ5p1MhlQ0V6kEkykMnMrYLt2e2g1HxvWHG28QOO9ZBly7U9AaA0mhzB6mlyqIlh7SAkVYOEgfzpRw6ofooCh+YKm7+svfMoE+VdGOmUj/3AVbBZP1hTd39LoDLmDep0QdkOlTmRZFnYE4xzsg7QzGYbjxT4YmU7mqRoqcDXPXAFXHK26ve8iU1fGbIW8YRYLJg8LQ8foNgorQIMUmu7bvijc2tquGTe25zAxRz+LsfPJXDRPYnjohtVoVFIVnna3Ya4jJwJKNp+Voo1T1I3pXSYQQBapcjlBfW2N9IZt5RhIO9tufJdYrV3tsZirxziKQxOW1ObhjBoK5aq7IiECgYEA5DQYzcJ+BinAI/mv0+NVQAI7HhyxEGY9zpU2X092VfoE2odrIIZmxjCpyKLJfBlW7RGWMaHGArcsN7KV9uS+7G8pqpoH0NQt4hV0hr7M9aikRn/Cogs0R0idTB/BD1+N4xu5tJ6/ScptMM2QpcH2BfdL/FPNMOdx326tQ5GAktkCgYEAx+86Y8PVlgfy5RM/xpZi/G9xQK8U2lSkRyq9r5KygzFvh49TG5VKMXfpjSHCCScuohXmmyauN9GqjwfUih4U7V2G7CUteUsn9uVFBs9oI8C0iUniBHpfw+J30Vnk3oexAlx+O+G1x/keaicucklj9jiJXV8nLLZZpwA6JsVVQq0CgYA0r96GKwBjteH5rF0vA46cL6eshR1QVvqwJn0eNj9xDD9woNFX+g/idDvO6d6w+zy5kVTm8p2/5MCdv2qmTRvThLrO+68QRNyvFRsJPSYfFaeP61FpjkKsrpQ6jh5JyVr981nF3Nz3UXgyS1O5IXYG3Ji7kjGOt/DvkCMKtZ5EyQKBgC/f8/5hrWpQmvQJiPL3ndpOoTLY+chSDkhQTr2VBhPuKYOvaToopGx2B0HZPIpOjJcZYW/zcjJBjXLUcCtmiqJQC/fN6zapLXesbLv1vD0bbt+VBn8GApp4RzKlMO2sVlF3bm7OXGX3HDZVDYUmV75kyVfHai0FuTAJGOOhywm9AoGAIrkN6Q5yU35IdYMLUd9oOu7R+H4RsKOmRBUV0cjQXE+0nHPPjUfHVGKedK3zpG4DZxmUXygtlZhNXU+eOzAh2XVrkccusWFw+SCkj7TK5qVKuNLIZ6rwoNstD6U9EL59yxcaI/GJiIzNMBiSKqUmzrehMtd0zUOisqHL19Z76VE=";
+    // 支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
+    public static String alipay_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jAvrKL+xJDO7p9PazXMo+H1fUXmjqHCTSeju+OJ2ow5GrbU7v3ZixF/FhD1B/bDt5Um0NCJbShBxbfrIKdsnP43g7wOYVhR8qcZ0iJEdWvZI6yO6+5mm9NsZc6WdNMuhYhm8qVyYfG/L6nb9fmZJk/nAbtD0bkz5YhW/6wk4taEJDtIFa0nz9qpN98exNzFfWmshaWvQoQAPnrfY0isg1Q7EVXFnMOQfOANIi38Axk/DV+MleW1gcTX/WmvfhChssgS+CyEEqyU9S0g9SG2Ii7yojObD9FmZcR7jYYE0g/KwSd/Xgr17h6BJtYZvVeRfexv4jKYKlBgizpBbsfsIQIDAQAB";
+    public static String notify_url = "http://localhost:8888/alipay/alipayReturnNotice";
+    public static String return_url = "http://localhost:8888/alipay/alipayReturnNotice";//localhost:8888/alipay/alipayReturnNotice
+    public static String sign_type = "RSA2";
+    public static String charset = "utf-8";
+    public static String gatewayUrl = "https://openapi.alipaydev.com/gateway.do";//注意：沙箱测试环境，正式环境为：https://openapi.alipay.com/gateway.do
+}
